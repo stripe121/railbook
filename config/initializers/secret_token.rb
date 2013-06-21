c21e9d6903c9e8f5cc9a6d140c6827c40a4fbdcb
stripe121 <stripe121@gmail.com>
2013-06-21 18:47:08 +0900

    初期状態をコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = 'f7899751b62846f17b5f4f327e2e6e9cd4e183f1c3642a7393e75d6f56b986427e5fc1b067a243bf6884a857e7f7d81c68b81f6d2575460188ebe89e4c9343fb'
